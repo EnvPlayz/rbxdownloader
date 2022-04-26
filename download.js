ed87c5e8390670a9037b0050c7ca6fa7cbdf2d6c
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2022-04-26 19:59:22 +0300

    Delete download.js

--- download.js
+++
@@ -1,7 +0,0 @@
-function download(resourceData,fileName,extension) {
-    const a = document.createElement('a');
-    a.rel = 'noopener'
-    a.href = resourceData;
-    a.setAttribute('download', `${fileName}.${extension}`);
-    click(a)
-}
